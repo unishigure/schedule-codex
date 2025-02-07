b4c280b7c954c200130db9f2a8b627e1377361bf
unishigure <uni@seanut.app>
2025-02-07 19:35:20 +0900

    feat: add health check cron job

--- src/cron.ts
+++ src/cron.ts
@@ -5,17 +5,29 @@ import type { app } from "./api";
 
 const api = treaty<typeof app>(process.env.API_URL ?? "");
 
-new Elysia().use(
-  cron({
-    name: "reminder-event",
-    pattern: "0 0 * * *",
-    timezone: "Asia/Tokyo",
-    async run() {
-      console.log(new Date(), "Cron job started");
-      await api.today.post();
-      console.log(new Date(), "Cron job finished");
-    },
-  }),
-);
+new Elysia()
+  .use(
+    cron({
+      name: "reminder-event",
+      pattern: "0 0 * * *",
+      timezone: "Asia/Tokyo",
+      async run() {
+        console.log(new Date(), "Cron job started");
+        await api.today.post();
+        console.log(new Date(), "Cron job finished");
+      },
+    }),
+  )
+  .use(
+    cron({
+      name: "health-check",
+      pattern: "*/5 * * * *",
+      timezone: "Asia/Tokyo",
+      async run() {
+        const res = await api.health.get();
+        console.log(new Date(), res);
+      },
+    }),
+  );
 
 console.log("Cron job is running");
